4ddbb15e242500981bf89c6c0d18c94c64fa3706
Nitesh <nitesh@cambiumretail.com>
2026-02-27 17:30:03 +0530

    Open CORS fully

--- app/api/main.py
+++ app/api/main.py
@@ -38,6 +38,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://am-replenishment-1.onrender.com",
+        "https://am-replenishment.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
